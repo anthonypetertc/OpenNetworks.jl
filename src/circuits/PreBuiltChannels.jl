76a3e100b29397285534158107b420a0bc1a571b
tony <anthony.peter.thompson@pm.me>
2025-04-23 16:49:42 +0100

    Added docstrings and tidied imports of PreBuiltChannels.jl

--- src/circuits/PreBuiltChannels.jl
+++ src/circuits/PreBuiltChannels.jl
@@ -1,10 +1,9 @@
 module PreBuiltChannels
 export depolarizing, dephasing
+
 using ITensors
-using ITensorMPS
 using ITensorsOpenSystems:
-    Vectorization, ITensorsOpenSystems, Vectorization.VectorizedDensityMatrix
-using ITensorNetworks
+    Vectorization.VectorizedDensityMatrix
 using OpenNetworks: Channels.Channel, VDMNetworks.VDMNetwork
 
 
@@ -37,6 +36,20 @@ function _kraus_depolarizing2(p:: Real, sites:: Vector{<: ITensors.Index{}}):: V
     return kraus_maps
 end
 
+"""
+    depolarizing(p::Real, sites::Vector{<:ITensors.Index{}})::Channel
+
+    Arguments
+    p::Real
+        The depolarizing probability.
+    sites::Vector{<:ITensors.Index{}}
+        The sites on which the depolarizing channel acts.
+    
+    Returns a Channel representing the depolarizing channel on the given sites.
+    Only implemented for the one or two qubit case.
+
+"""
+
 function depolarizing(p::Real, sites::Vector{<:ITensors.Index{}})::Channel
     #= Creates a depolarizing channel for a given density matrix. =#
 
@@ -63,10 +76,30 @@ function depolarizing(p::Real, sites::Vector{<:ITensors.Index{}})::Channel
     end
 end
 
+"""
+    depolarizing(p::Real, sites::Vector{<:ITensors.Index{}}, rho::VectorizedDensityMatrix,
+        unvectorizedsiteinds::Vector{<:ITensors.Index{}})::Channel
+
+    Arguments
+    p::Real
+        The depolarizing probability.
+    sites::Vector{<:ITensors.Index{}}
+        The sites on which the depolarizing channel acts.
+    rho::VectorizedDensityMatrix
+        The density matrix to which the depolarizing channel is applied.
+    unvectorizedsiteinds::Vector{<:ITensors.Index{}}
+        The unvectorized site indices of the density matrix.
+
+    Returns a Channel representing the depolarizing channel on the given sites.
+    The sites must be from the unvectorisedsiteinds, and the Channel will act
+    on the corresponding vectorized sites.
+
+"""
+
 function depolarizing(
     p::Real,
     sites::Vector,
-    rho::Vectorization.VectorizedDensityMatrix,
+    rho::VectorizedDensityMatrix,
     unvectorizedsiteinds::Vector{<:ITensors.Index{}},
 )::Channel
     #= Creates a depolarizing channel for a given density matrix. =#
@@ -93,6 +126,23 @@ function depolarizing(
         )
     end
 end
+
+"""
+    depolarizing(p::Real, sites::Vector{<:ITensors.Index{}}, rho::VDMNetwork{V})::Channel where {V}
+
+    Arguments
+    p::Real
+        The depolarizing probability.
+    sites::Vector{<:ITensors.Index{}}
+        The sites on which the depolarizing channel acts.
+    rho::VDMNetwork{V}
+        The density matrix to which the depolarizing channel is applied.
+
+    Returns a Channel representing the depolarizing channel on the given sites,
+    the sites must be from the unvectorisedsiteinds, and the Channel will act
+    on the corresponding vectorized sites.
+
+"""
 
 
 function depolarizing(p::Real, sites::Vector, rho::VDMNetwork{V})::Channel where {V}
@@ -130,15 +180,62 @@ function _kraus_dephasing(p:: Real, site:: ITensors.Index):: Vector{ITensor}
     return [M0, M1]
 end
 
+"""
+    dephasing(p::Real, site::ITensors.Index)::Channel
+
+    Arguments
+    p::Real
+        The dephasing probability.
+    site::ITensors.Index{}
+        The site on which the dephasing channel acts.
+
+    Returns a Channel representing the dephasing channel on the given site.
+"""
+
 function dephasing(p::Real, site::ITensors.Index)::Channel
     kraus_maps = _kraus_dephasing(p, site)
     return Channel("dephasing", kraus_maps)
 end
 
+"""
+    dephasing(p::Real, site::ITensors.Index, ρ::VDMNetwork{V})::Channel where {V}
+
+    Arguments
+    p::Real
+        The dephasing probability.
+    site::ITensors.Index{}
+        The site on which the dephasing channel acts.
+    ρ::VDMNetwork{V}
+        The density matrix to which the dephasing channel is applied.
+
+    Returns a Channel representing the dephasing channel on the given site.
+    The site must be from the unvectorizedsiteinds, and the Channel will act
+    on the corresponding vectorized sites.
+"""
+
 function dephasing(p::Real, site::ITensors.Index, ρ::VDMNetwork{V})::Channel where {V}
     kraus_maps = _kraus_dephasing(p ,site)
     return Channel("dephasing", kraus_maps, ρ)
 end
+
+"""
+    dephasing(p::Real, site::ITensors.Index, ρ::VectorizedDensityMatrix,
+        unvectorizedsites::Vector{<:ITensors.Index{}},)::Channel
+
+    Arguments
+    p::Real
+        The dephasing probability.
+    site::ITensors.Index{}
+        The site on which the dephasing channel acts.
+    ρ::VectorizedDensityMatrix
+        The density matrix to which the dephasing channel is applied.
+    unvectorizedsites::Vector{<:ITensors.Index{}}
+        The unvectorized site indices of the density matrix.
+
+    Returns a Channel representing the dephasing channel on the given site.
+    The site must be from the unvectorizedsiteinds, and the Channel will act
+    on the corresponding vectorized sites.
+"""
 
 function dephasing(
     p::Real,
@@ -150,52 +247,4 @@ function dephasing(
     return Channel("dephasing", kraus_maps, ρ, unvectorizedsites)
 end
 
-end; #module
-
-#=
-function depolarizing(
-    p::Real, sites::Vector, rho::MPS, unvectorizedsites::Vector{<:ITensors.Index{}}
-)::Channel
-    #= Creates a depolarizing channel for a given density matrix.=#
-
-    if !(0 <= p <= 1)
-        throw("parameter p must be between 0 and 1.")
-    end
-    for site in sites
-        @assert hastags(site, "Qubit") "Depolarizing channel only implemented for Qubits."
-    end
-
-    k = length(sites)
-    if k == 1
-        const0 = sqrt(1 - 3 * p / 4)
-        constσ = sqrt(p / 4)
-        kraus_maps = [
-            constσ * op("σx", sites[1]),
-            constσ * op("σy", sites[1]),
-            constσ * op("σz", sites[1]),
-            const0 * op("Id", sites[1]),
-        ]
-        return Channel("depolarizing", kraus_maps, rho, unvectorizedsites)
-
-    elseif k == 2
-        const0 = sqrt(1 - 15 * p / 16)
-        constσ = sqrt(p / 16)
-        pauli1 = [
-            op("σx", sites[1]), op("σy", sites[1]), op("σz", sites[1]), op("Id", sites[1])
-        ]
-        pauli2 = [
-            op("σx", sites[2]), op("σy", sites[2]), op("σz", sites[2]), op("Id", sites[2])
-        ]
-        kraus_maps = [constσ * x * y for x in pauli1 for y in pauli2]
-        replace!(
-            kraus_maps,
-            Pair(constσ * pauli1[end] * pauli2[end], const0 * pauli1[end] * pauli2[end]),
-        )
-        return Channel("depolarizing", kraus_maps, rho, unvectorizedsites)
-
-    else
-        throw(
-            "Depolarizing channel for more than two qubit gates has not been implemented."
-        )
-    end
-end=#
+end; # module
--- test/full/full_test_evolution.jl
+++ test/full/full_test_evolution.jl
@@ -34,7 +34,7 @@ vsites = ITensorNetworks.siteinds("QubitVec", g)
     )
     noise_model = NoiseModels.NoiseModel(Set([noise_instruction]), sites, vsites)
     noisy_circuit = NoisyCircuits.NoisyCircuit(qc, noise_model)
-    evolved_ρ = NoisyCircuits.apply(ρ, noisy_circuit; maxdim=128, cutoff=1e-16)
+    evolved_ρ = ITensorNetworks.apply(ρ, noisy_circuit; maxdim=128, cutoff=1e-16)
     @test ITensorNetworks.inner(evolved_ρ.network, ρ.network) ≈ 1.0
 end;
 
