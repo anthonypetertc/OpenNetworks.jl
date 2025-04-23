57e3cffba41c5f18f23158ccffb9108b286dec6f
tony <anthony.peter.thompson@pm.me>
2025-04-23 12:33:16 +0100

    Added docstrings and tidied imports in GraphUtils.jl

--- src/utilities/GraphUtils.jl
+++ src/utilities/GraphUtils.jl
@@ -3,11 +3,19 @@ module GraphUtils
 export extract_adjacency_graph, named_ring_graph, linenetwork, islinenetwork
 
 using NamedGraphs: NamedGraphs, add_edges!, NamedGraph
-using Graphs
 using ITensors
 using ITensorNetworks: IndsNetwork, vertices, neighbors
-using JSON
-using OpenNetworks: Gates.Gate, OpenNetworks, Utils.findindextype
+using OpenNetworks: Gates.Gate
+
+"""
+    extract_adjacency_graph(qc::Vector{Gate})::NamedGraphs.NamedGraph
+
+    Arguments
+    qc::Vector{Gate}
+        The quantum circuit represented as a vector of Gate objects.
+
+    Returns a NamedGraph representing the adjacency graph of the quantum circuit.
+"""
 
 function extract_adjacency_graph(qc::Vector{Gate})::NamedGraphs.NamedGraph
     all_qubits = Vector{Int64}()
@@ -27,12 +35,32 @@ function extract_adjacency_graph(qc::Vector{Gate})::NamedGraphs.NamedGraph
     return G
 end
 
+"""
+    named_ring_graph(n::Integer)::NamedGraphs.NamedGraph
+
+    Arguments
+    n::Integer
+        The number of vertices in the ring graph.
+
+    Returns a NamedGraph representing a ring graph with n vertices.
+"""
+
 function named_ring_graph(n::Integer)
     G = NamedGraph([i for i in 0:(n - 1)])
     add_edges!(G, [i => i + 1 for i in 0:(n - 2)])
     add_edges!(G, [n - 1 => 0])
     return G
 end
+
+"""
+    linegraph(sites::Vector{<:ITensors.Index{}})::NamedGraphs.NamedGraph
+
+    Arguments
+    sites::Vector{<:ITensors.Index{}}
+        The sites to be connected in a line graph.
+
+    Returns a NamedGraph representing a line graph with the given sites.
+"""
 
 function linegraph(sites::Vector{<:ITensors.Index{}})::NamedGraphs.NamedGraph
     G = NamedGraphs.NamedGraph([i for i in 1:length(sites)])
@@ -41,6 +69,16 @@ function linegraph(sites::Vector{<:ITensors.Index{}})::NamedGraphs.NamedGraph
     end
     return G
 end
+
+"""
+    islinegraph(g::NamedGraphs.NamedGraph)::Bool
+
+    Arguments
+    g::NamedGraphs.NamedGraph
+        The graph to be checked.
+
+    Returns true if the graph is a line graph, false otherwise.
+"""
 
 function islinegraph(g::NamedGraphs.NamedGraph):: Bool
     verts = vertices(g)
@@ -54,6 +92,19 @@ function islinegraph(g::NamedGraphs.NamedGraph):: Bool
     end
 end
 
+"""
+    linenetwork(sites::Vector{<:ITensors.Index{}}, sitetype::String="Qubit")::IndsNetwork
+
+    Arguments
+    sites::Vector{<:ITensors.Index{}}
+        The sites to be connected in a line network.
+    sitetype::String
+        The type of the sites (default is "Qubit").
+
+    Returns an IndsNetwork representing a line network with the given sites.
+
+"""
+
 function linenetwork(
     sites::Vector{<:ITensors.Index{}}, sitetype::String="Qubit"
 )::IndsNetwork
@@ -64,6 +115,21 @@ function linenetwork(
     end
     return indsnetwork
 end
+
+"""
+    linenetwork(sites::Vector{<:ITensors.Index{}}, qc::Vector{Gate}, sitetype::String="Qubit")::IndsNetwork
+
+    Arguments
+    sites::Vector{<:ITensors.Index{}}
+        The sites to be connected in a line network.
+    qc::Vector{Gate}
+        The quantum circuit represented as a vector of Gate objects.
+    sitetype::String
+        The type of the sites (default is "Qubit").
+
+    Returns an IndsNetwork representing a line network with the vertices given by qubit numbers
+    from the quantum circuit, and the site indices given by the sites.
+"""
 
 function linenetwork(
     sites:: Vector{<:ITensors.Index{}},
@@ -83,21 +149,34 @@ function linenetwork(
     return indsnetwork
 end
 
+"""
+    islinenetwork(sites::IndsNetwork)::Bool
+
+    Arguments
+    sites::IndsNetwork
+        The IndsNetwork to be checked.
+
+    Returns true if the IndsNetwork is a network with 1-d line connectivity, false otherwise.
+"""
+
 function islinenetwork(sites::IndsNetwork)::Bool
     return islinegraph(sites.data_graph.underlying_graph)
 end
 
 
-#=
-function islinenetwork_old(sites::IndsNetwork)::Bool
-    T = findindextype(sites)
-    siteindices = Vector{T}()
-    for v in vertices(sites)
-        push!(siteindices, first(sites[v]))
-    end
-    lg = linegraph(siteindices)
-    return sites.data_graph.underlying_graph == lg
-end=#
+"""
+    siteinds(tag::String, qc::Vector{Gate})::Vector{<:Index}
+
+    Arguments
+    tag::String
+        The tag to be used for the indices.
+    qc::Vector{Gate}
+        The quantum circuit represented as a vector of Gate objects.
+
+    (Only implemented for circuits with 1-d connectivity on a line)
+    Returns a vector of site indices with the given tag, where each index is labelled by the qubit number
+    from the quantum circuit.
+"""
 
 function ITensors.siteinds(tag::String, qc:: Vector{Gate})::Vector{<:Index}
     g = extract_adjacency_graph(qc)
